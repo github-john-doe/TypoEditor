e8bbf27c785ff46097039d5b0e65eae158380600
John Doe <github.john.doe@outlook.com>
2017-08-10 07:03:23 -0700

    Missed file

---
+++ TypoEditor/OccurrenceItemViewModel.cs
@@ -0,0 +1,46 @@
+﻿namespace TypoEditor
+{
+    using System;
+    using System.Diagnostics;
+    using System.Windows.Input;
+
+    public class OccurrenceItemViewModel
+    {
+        public string Name { get; set; }
+
+        public ICommand DoubleClickCommand
+        {
+            get
+            {
+                return new ViewFileCommand(this);
+            }
+        }
+
+        private class ViewFileCommand : ICommand
+        {
+            private OccurrenceItemViewModel item;
+
+            public ViewFileCommand(OccurrenceItemViewModel item)
+            {
+                this.item = item;
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object parameter)
+            {
+                Process process = new Process();
+                ProcessStartInfo processStartInfo = new ProcessStartInfo();
+                processStartInfo.FileName = @"C:\windows\system32\notepad.exe";
+                processStartInfo.Arguments = this.item.Name;
+                process.StartInfo = processStartInfo;
+                process.Start();
+            }
+        }
+    }
+}
